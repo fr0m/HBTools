c50a220356f136b54821f4aeaa700194ba298279
herbert <fr0m@qq.com>
2021-12-09 10:07:46 +0800

    Create getHBKeys.js

---
+++ getHBKeys.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         getHBKeys
+// @namespace    https://github.com/fr0m/HBTools
+// @version      0.1
+// @description  get Humble Bundle keys in console
+// @author       fr0m
+// @updateURL    https://github.com/fr0m/HBTools/raw/master/hbMeta.js
+// @downloadURL  https://github.com/fr0m/HBTools/raw/master/getHBKeys.js
+// @match        https://www.humblebundle.com/downloads*
+// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+  
+    const keyNodeSelector = '.key-redeemer';
+    const keyNameSelector = '.heading-text h4';
+    const keySelector = '.keyfield-value';
+
+    const getKeys = () => {
+      const keyNodes = document.querySelectorAll(keyNodeSelector);
+      let keys = [];
+      keyNodes.forEach(node => {
+        const name = node.querySelector(keyNameSelector).innerText;
+        const key = node.querySelector(keySelector).innerText;
+        
+        keys.push({
+          name,
+          key
+        });
+      });
+      
+      console.log(keys.map(({ name, key }) => `${name}: ${key}`).join('\n'));
+      return keys;
+    }
+    
+    window.getHBKeys = getKeys;
+})();
